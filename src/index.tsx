f6908bc6c0eb9732bc7124b1545903bb94fd1ff3
Rico Schmidt <rico.schmidt@namics.com>
2018-10-22 18:02:15 +0200

    Add sizing breakpoints & update readme

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
+import "./components/sizingbreakpoints.scss";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
